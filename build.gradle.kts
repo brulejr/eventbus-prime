f6b60c95821f61914209eac1d61dd5e67febb7f8
Jon Brule <brulejr@tgmail.com>
2026-04-10 07:29:43 -0400

    Refactors build to support local KSB Commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,38 @@
+val ksbCommonsVersion: String by project
+val projectVersion: String by project
+
+val useLocalKsbCommons: Boolean =
+    providers.gradleProperty("useLocalKsbCommons")
+        .map { it.toBoolean() }
+        .orElse(true)
+        .get()
+
 plugins {
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.spring") version "2.1.21"
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.spring") version "2.2.10"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.jrb.labs"
 version = "0.0.1-SNAPSHOT"
+
+/**
+ * Resolve the project version:
+ *
+ * - In CI on a tag build, GITHUB_REF_NAME will be something like "v0.3.1".
+ *   We strip the leading "v" and use "0.3.1" as the version.
+ * - Otherwise, fall back to projectVersion from gradle.properties.
+ */
+version = System.getenv("GITHUB_REF_NAME")
+    ?.let { refName ->
+        if (refName.matches(Regex("""v\d+\.\d+\.\d+"""))) {
+            refName.removePrefix("v")
+        } else {
+            projectVersion
+        }
+    }
+    ?: projectVersion
 
 java {
     toolchain {
@@ -16,9 +42,30 @@ java {
 
 repositories {
     mavenCentral()
+    if (!useLocalKsbCommons) {
+        maven {
+            url = uri("https://maven.pkg.github.com/brulejr/ksb-commons")
+            credentials {
+                username = findProperty("gpr.user") as String?
+                    ?: System.getenv("GITHUB_PACKAGES_USER")
+                            ?: System.getenv("GITHUB_ACTOR")
+                            ?: "brulejr"
+                password = findProperty("gpr.key") as String?
+                    ?: System.getenv("GITHUB_PACKAGES_TOKEN")
+                            ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 dependencies {
+    if (useLocalKsbCommons) {
+        implementation(platform("io.jrb.labs:ksb-dependency-bom"))
+    } else {
+        implementation(platform("io.jrb.labs:ksb-dependency-bom:$ksbCommonsVersion"))
+    }
+    implementation("io.jrb.labs:ksb-commons-ms-core")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -27,8 +74,9 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.assertj:assertj-core")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.mockk)
 }
 
 tasks.withType<Test> {
@@ -40,4 +88,9 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
+
+springBoot {
+    // info.app.version will now be aligned with tag-derived project.version
+    buildInfo()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "eventbus-prime"
-includeBuild("vendor/ksb-commons")
+
+val useLocalKsbCommons =
+    providers.gradleProperty("useLocalKsbCommons")
+        .map { it.toBoolean() }
+        .orElse(true)
+        .get()
+
+val ksbCommonsDir = file("vendor/ksb-commons")
+
+if (useLocalKsbCommons) {
+    if (ksbCommonsDir.exists()) {
+        includeBuild(ksbCommonsDir)
+    } else {
+        logger.warn("useLocalKsbCommons=true but ./vendor/ksb-commons is missing; falling back to published dependency")
+    }
+}
